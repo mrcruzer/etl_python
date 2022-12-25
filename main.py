a1b35c604da64297f882d50e9db1d7a105a9e152
Lucia Gabriela Figuereo <Luciagfiguereo@gmail.com>
2022-12-25 19:41:48 -0400

    first

---
+++ main.py
@@ -0,0 +1,9 @@
+import sqlalchemy
+from sqlalchemy import create_engine
+import pandas as pd 
+from sqlalchemy.orm import sessionmaker
+import requests
+import json
+from datetime import datetime
+import datetime
+import pymysql
